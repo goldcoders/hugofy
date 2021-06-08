67b0591e7b8c080c16b3d23dc005cbd799e3b38a
Goldcoders <goldcoders@protonmail.com>
2021-06-09 06:38:02 +0800

    add Social Fields

---
+++ src/social/github_field.ts
@@ -0,0 +1,30 @@
+import { CmsFieldBase, CmsFieldObject } from 'netlify-cms-core';
+
+type Field= CmsFieldBase & CmsFieldObject;
+let field:Field = {
+    "label": "Github",
+    "name": "github",
+    "widget": "object",
+    "fields": [
+        {
+            "label": "Link",
+            "name": "link",
+            "widget": "string",
+            "pattern": [
+                "(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})",
+                "ONLY VALID URL ALLOWED!"
+            ]
+        },
+        {
+            "label": "Icon",
+            "name": "icon",
+            "widget": "image",
+            "required": false,
+            "default": "/images/github.svg"
+        }
+    ]
+};
+export default field;
+
+
+
---
+++ src/social/youtube_field.ts
@@ -0,0 +1,32 @@
+import { CmsFieldBase, CmsFieldObject } from 'netlify-cms-core';
+
+type Field= CmsFieldBase & CmsFieldObject;
+let field:Field = {
+    "label": "Youtube",
+    "name": "youtube",
+    "widget": "object",
+    "fields": [
+        {
+            "label": "Link",
+            "name": "link",
+            "widget": "string",
+            "pattern": [
+                "(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})",
+                "ONLY VALID URL ALLOWED!"
+            ]
+        },
+        {
+            "label": "Icon",
+            "name": "icon",
+            "widget": "image",
+            "required": false,
+            "default": "/images/youtube.svg"
+        }
+    ]
+
+};
+export default field;
+
+
+
+
