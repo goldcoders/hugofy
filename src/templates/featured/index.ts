46b2f2c5f29e7709b88baab20ad2d0571dfba27a
Goldcoders <goldcoders@protonmail.com>
2021-06-09 07:05:31 +0800

    added featured templates

---
+++ src/templates/featured/index.ts
@@ -0,0 +1,7 @@
+import Featured3Col from './3col';
+import FeaturedCenter2By2 from './center_2x2';
+import FeaturedOffset2By2Grid from './offset_2x2_grid';
+import FeatureImageImage from './split_image';
+
+export { FeatureImageImage, FeaturedCenter2By2, FeaturedOffset2By2Grid, Featured3Col };
+
