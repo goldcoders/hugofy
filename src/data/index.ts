3247024fa6033184ba54b964c93a099dc9e708b0
Goldcoders <goldcoders@protonmail.com>
2021-06-09 06:36:53 +0800

    add other data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,9 @@
 import CartData from './cart';
+import CategoryData from './categories';
 import ColorPickerData from './colorpicker';
 import FontData from './font';
+import LegalitiesData from './legalities';
 import SocialData from './social';
-export { CartData, SocialData, FontData, ColorPickerData };
+import TagsData from './tags';
+export { CartData, SocialData, FontData, ColorPickerData, LegalitiesData, TagsData, CategoryData };
 
