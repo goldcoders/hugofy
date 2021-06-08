d5cb3544d82c8e2d16a094f21b89e3230e5a8f43
Goldcoders <goldcoders@protonmail.com>
2021-06-09 06:37:17 +0800

    add tag data

---
+++ src/data/tags.ts
@@ -0,0 +1,29 @@
+import { CmsCollectionFile } from 'netlify-cms-core';
+
+let config:CmsCollectionFile = {
+    "file": "/data/tags.yaml",
+    "label": "Tag List",
+    "name": "tag_list",
+    "fields": [
+        {
+            "label": "Tag List",
+            "name": "tags",
+            "widget": "list",
+            "required": false,
+            "collapsed": true,
+            "label_singular": "Tag",
+            "fields": [
+                {
+                    "label": "Name",
+                    "name": "name",
+                    "widget": "string",
+                    "pattern": [
+                        "^[a-z0-9]+(?:-[a-z0-9]+)*$",
+                        "Letters, Numbers and Dash(-) ONLY"
+                    ]
+                }
+            ]
+        }
+    ]
+};
+export default config;
