26c2c9f90c7bfecaf3fcf9e66c23939e8816f4d9
Goldcoders <goldcoders@protonmail.com>
2021-06-09 06:37:07 +0800

    add social fields

--- src/data/social.ts
+++ src/data/social.ts
@@ -1,4 +1,5 @@
 import { CmsCollectionFile } from 'netlify-cms-core';
+import { fb_field, github_field, instagram_field, linkedin_field, twitter_field, youtube_field } from '../social';
 
 let config:CmsCollectionFile = {
     "file": "/data/social_links.yaml",
@@ -13,144 +14,12 @@ let config:CmsCollectionFile = {
             "collapsed": true,
             "label_singular": "Social Network",
             "types": [
-                {
-                    "label": "Facebook",
-                    "name": "facebook",
-                    "widget": "object",
-                    "fields": [
-                        {
-                            "label": "Link",
-                            "name": "link",
-                            "widget": "string",
-                            "pattern": [
-                                "(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})",
-                                "ONLY VALID URL ALLOWED!"
-                            ]
-                        },
-                        {
-                            "label": "Icon",
-                            "name": "icon",
-                            "widget": "image",
-                            "required": false,
-                            "default": "/images/facebook.svg"
-                        }
-                    ]
-                },
-                {
-                    "label": "Instagram",
-                    "name": "instagram",
-                    "widget": "object",
-                    "fields": [
-                        {
-                            "label": "Link",
-                            "name": "link",
-                            "widget": "string",
-                            "pattern": [
-                                "(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})",
-                                "ONLY VALID URL ALLOWED!"
-                            ]
-                        },
-                        {
-                            "label": "Icon",
-                            "name": "icon",
-                            "widget": "image",
-                            "required": false,
-                            "default": "/images/instagram.svg"
-                        }
-                    ]
-                },
-                {
-                    "label": "Twitter",
-                    "name": "twitter",
-                    "widget": "object",
-                    "fields": [
-                        {
-                            "label": "Link",
-                            "name": "link",
-                            "widget": "string",
-                            "pattern": [
-                                "(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})",
-                                "ONLY VALID URL ALLOWED!"
-                            ]
-                        },
-                        {
-                            "label": "Icon",
-                            "name": "icon",
-                            "widget": "image",
-                            "required": false,
-                            "default": "/images/twitter.svg"
-                        }
-                    ]
-                },
-                {
-                    "label": "Github",
-                    "name": "github",
-                    "widget": "object",
-                    "fields": [
-                        {
-                            "label": "Link",
-                            "name": "link",
-                            "widget": "string",
-                            "pattern": [
-                                "(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})",
-                                "ONLY VALID URL ALLOWED!"
-                            ]
-                        },
-                        {
-                            "label": "Icon",
-                            "name": "icon",
-                            "widget": "image",
-                            "required": false,
-                            "default": "/images/github.svg"
-                        }
-                    ]
-                },
-                {
-                    "label": "Youtube",
-                    "name": "youtube",
-                    "widget": "object",
-                    "fields": [
-                        {
-                            "label": "Link",
-                            "name": "link",
-                            "widget": "string",
-                            "pattern": [
-                                "(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})",
-                                "ONLY VALID URL ALLOWED!"
-                            ]
-                        },
-                        {
-                            "label": "Icon",
-                            "name": "icon",
-                            "widget": "image",
-                            "required": false,
-                            "default": "/images/youtube.svg"
-                        }
-                    ]
-                },
-                {
-                    "label": "LinkedIn",
-                    "name": "linkedin",
-                    "widget": "object",
-                    "fields": [
-                        {
-                            "label": "Link",
-                            "name": "link",
-                            "widget": "string",
-                            "pattern": [
-                                "(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})",
-                                "ONLY VALID URL ALLOWED!"
-                            ]
-                        },
-                        {
-                            "label": "Icon",
-                            "name": "icon",
-                            "widget": "image",
-                            "required": false,
-                            "default": "/images/linkedin.svg"
-                        }
-                    ]
-                }
+                fb_field,
+                instagram_field,
+                twitter_field,
+                youtube_field,
+                github_field,
+                linkedin_field
             ]
         }
     ]
