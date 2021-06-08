72c4f5f537768f4335a05bbf4eaf29875d245e0d
Goldcoders <goldcoders@protonmail.com>
2021-06-09 06:36:41 +0800

    refactor

--- src/collections/taxonomies.ts
+++ src/collections/taxonomies.ts
@@ -1,4 +1,5 @@
 import { CmsCollection } from 'netlify-cms-core';
+import { CategoryData, TagsData } from '../data';
 
 let taxonomies:CmsCollection= {
     "name": "taxonomies",
@@ -7,58 +8,8 @@ let taxonomies:CmsCollection= {
         "preview": false
     },
     "files": [
-        {
-            "file": "/data/tags.yaml",
-            "label": "Tag List",
-            "name": "tag_list",
-            "fields": [
-                {
-                    "label": "Tag List",
-                    "name": "tags",
-                    "widget": "list",
-                    "required": false,
-                    "collapsed": true,
-                    "label_singular": "Tag",
-                    "fields": [
-                        {
-                            "label": "Name",
-                            "name": "name",
-                            "widget": "string",
-                            "pattern": [
-                                "^[a-z0-9]+(?:-[a-z0-9]+)*$",
-                                "Letters, Numbers and Dash(-) ONLY"
-                            ]
-                        }
-                    ]
-                }
-            ]
-        },
-        {
-            "file": "/data/categories.yaml",
-            "label": "Category List",
-            "name": "category_list",
-            "fields": [
-                {
-                    "label": "Category List",
-                    "name": "categories",
-                    "required": false,
-                    "collapsed": true,
-                    "label_singular": "Category",
-                    "widget": "list",
-                    "fields": [
-                        {
-                            "label": "Name",
-                            "name": "name",
-                            "widget": "string",
-                            "pattern": [
-                                "^[a-z0-9]+(?:-[a-z0-9]+)*$",
-                                "Letters, Numbers and Dash(-) ONLY"
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }
+        CategoryData,
+        TagsData,
     ]
 };
 export { taxonomies };
