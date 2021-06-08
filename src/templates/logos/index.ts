1fd4f05acf517e18ed212c980cb62247379902a1
Goldcoders <goldcoders@protonmail.com>
2021-06-09 07:46:04 +0800

    added logos

---
+++ src/templates/logos/index.ts
@@ -0,0 +1,6 @@
+import LogoSimple from './simple';
+import LogosSplitGrid from './split_grid';
+import LogosWithHeaderCentered from './with_header_centered';
+
+export { LogosWithHeaderCentered, LogoSimple, LogosSplitGrid };
+
