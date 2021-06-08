9c11a72628648202bdecbadf45524c948b57ef8b
Goldcoders <goldcoders@protonmail.com>
2021-06-09 06:36:09 +0800

    add legalities data

--- src/collections/legalities_page.ts
+++ src/collections/legalities_page.ts
@@ -1,4 +1,5 @@
 import { CmsCollection } from 'netlify-cms-core';
+import { LegalitiesData } from '../data';
 
 let legalities_page:CmsCollection= {
     "name": "legalities",
@@ -7,95 +8,7 @@ let legalities_page:CmsCollection= {
         "preview": false
     },
     "files": [
-        {
-            "file": "/data/legalities.yaml",
-            "label": "Configure Legalities Content",
-            "name": "legalities",
-            "fields": [
-                {
-                    "label": "Business Name",
-                    "name": "business_name",
-                    "widget": "string"
-                },
-                {
-                    "label": "Owner's Name",
-                    "name": "owner",
-                    "widget": "string"
-                },
-                {
-                    "label": "Business Line",
-                    "name": "business_line",
-                    "widget": "string"
-                },
-                {
-                    "label": "Busines Address",
-                    "name": "business_address",
-                    "widget": "string"
-                },     
-                {
-                    "label": "Certificates",
-                    "name": "certificates",
-                    "required": false,
-                    "collapsed": true,
-                    "label_singular": "Certificate",
-                    "widget": "list",
-                    "summary": "{{fields.name}}",
-                    "types": [
-                        {
-                            "label": "External Link",
-                            "name": "link",
-                            "widget": "object",
-                            "fields": [
-                                {
-                                    "label": "Certificate Name",
-                                    "name": "name",
-                                    "widget": "string"
-                                },
-                                {
-                                    "label": "External URL",
-                                    "name": "url",
-                                    "widget": "string"
-                                }
-                            ]
-                        },
-                        {
-                            "label": "Image Upload",
-                            "name": "image",
-                            "widget": "object",
-                            "fields": [
-                                {
-                                    "label": "Certificate Name",
-                                    "name": "name",
-                                    "widget": "string"
-                                },
-                                {
-                                    "label": "Image",
-                                    "name": "url",
-                                    "widget": "image"
-                                }
-                            ]
-                        },
-                        {
-                            "label": "File Upload",
-                            "name": "file",
-                            "widget": "object",
-                            "fields": [
-                                {
-                                    "label": "Certificate Name",
-                                    "name": "name",
-                                    "widget": "string"
-                                },
-                                {
-                                    "label": "File",
-                                    "name": "url",
-                                    "widget": "file"
-                                }
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }
+       LegalitiesData 
     ]
 };
 export { legalities_page };
