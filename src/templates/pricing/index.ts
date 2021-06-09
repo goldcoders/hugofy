2185ee60d4f5b288a58d5bde68dfab4fc90e144c
Goldcoders <goldcoders@protonmail.com>
2021-06-09 11:47:51 +0800

    added pricing tiers

---
+++ src/templates/pricing/index.ts
@@ -0,0 +1,6 @@
+import PricingSingle from './single';
+import PricingThreeTier from './three-tier';
+import PricingTwoTier from './two-tier';
+
+export { PricingSingle, PricingTwoTier, PricingThreeTier };
+
