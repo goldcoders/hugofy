ae22b4168ebf05bda88efa270bb65484c4f418aa
Goldcoders <goldcoders@protonmail.com>
2021-06-10 09:07:50 +0800

    added default value if not ENV is not defined for some reason

--- src/cms.ts
+++ src/cms.ts
@@ -4,16 +4,16 @@ import collections from './collections/index';
 import './events';
 import './shortcodes';
 
-const local_backend: boolean = JSON.parse(import.meta.env.SNOWPACK_PUBLIC_BACKEND);
-const backendType: CmsBackendType = import.meta.env.SNOWPACK_PUBLIC_BACKEND_TYPE;
-const media_folder: string = `${import.meta.env.SNOWPACK_PUBLIC_MEDIA_FOLDER}`;
-const repo: string = `${import.meta.env.SNOWPACK_PUBLIC_REPO}`;
+const local_backend: boolean = !!import.meta.env.SNOWPACK_PUBLIC_BACKEND;
+const backendType: CmsBackendType = import.meta.env.SNOWPACK_PUBLIC_BACKEND_TYPE || 'git-gateway';
+const media_folder: string = `${import.meta.env.SNOWPACK_PUBLIC_MEDIA_FOLDER}` || 'static/images';
+const repo: string = `${import.meta.env.SNOWPACK_PUBLIC_REPO}` || 'goldcoders/hugo-dusk';
 const branch: string = `${import.meta.env.SNOWPACK_PUBLIC_BRANCH}` || 'main';
-const public_folder: string = `${import.meta.env.SNOWPACK_PUBLIC_PUBLIC_FOLDER}`;
-const site_url: string = `${import.meta.env.SNOWPACK_PUBLIC_SITE_URL}`;
-const site_domain: string = `${import.meta.env.SNOWPACK_PUBLIC_DOMAIN}`;
-const display_url: string = `${import.meta.env.SNOWPACK_PUBLIC_DISPLAY_URL}`;
-const show_preview_links: boolean = JSON.parse(import.meta.env.SNOWPACK_PUBLIC_SHOW_PREVIEW_LINKS);
+const public_folder: string = `${import.meta.env.SNOWPACK_PUBLIC_PUBLIC_FOLDER}` || '/images';
+const site_url: string = `${import.meta.env.SNOWPACK_PUBLIC_SITE_URL}` || 'https://goldcoders.dev';
+const site_domain: string = `${import.meta.env.SNOWPACK_PUBLIC_DOMAIN}` || 'goldcoders.dev';
+const display_url: string = `${import.meta.env.SNOWPACK_PUBLIC_DISPLAY_URL}` || 'https://goldcoders.dev';
+const show_preview_links: boolean = !!import.meta.env.SNOWPACK_PUBLIC_SHOW_PREVIEW_LINKS;
 
 let backend: CmsBackend = {
     name: backendType,
@@ -33,5 +33,5 @@ let config:CmsConfig = {
     show_preview_links,
     collections
 }
-
+console.log(config);
 CMS.init({config});
--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,16 @@ import collections from './collections/index';
 import './events';
 import './shortcodes';
 
-const local_backend: boolean = process.env.SNOWPACK_PUBLIC_BACKEND;
-const backendType: CmsBackendType = process.env.SNOWPACK_PUBLIC_BACKEND_TYPE;
-const media_folder: string = `${process.env.SNOWPACK_PUBLIC_MEDIA_FOLDER}`;
-const repo: string = `${process.env.SNOWPACK_PUBLIC_REPO}`;
+const local_backend: boolean = !!process.env.SNOWPACK_PUBLIC_BACKEND;
+const backendType: CmsBackendType = process.env.SNOWPACK_PUBLIC_BACKEND_TYPE || 'git-gateway';
+const media_folder: string = `${process.env.SNOWPACK_PUBLIC_MEDIA_FOLDER}` || 'static/images';
+const repo: string = `${process.env.SNOWPACK_PUBLIC_REPO}` || 'goldcoders/hugo-dusk';
 const branch: string = `${process.env.SNOWPACK_PUBLIC_BRANCH}` || 'main';
-const public_folder: string = `${process.env.SNOWPACK_PUBLIC_PUBLIC_FOLDER}`;
-const site_url: string = `${process.env.SNOWPACK_PUBLIC_SITE_URL}`;
-const site_domain: string = `${process.env.SNOWPACK_PUBLIC_DOMAIN}`;
-const display_url: string = `${process.env.SNOWPACK_PUBLIC_DISPLAY_URL}`;
-const show_preview_links: boolean = process.env.SNOWPACK_PUBLIC_SHOW_PREVIEW_LINKS;
+const public_folder: string = `${process.env.SNOWPACK_PUBLIC_PUBLIC_FOLDER}` || '/images';
+const site_url: string = `${process.env.SNOWPACK_PUBLIC_SITE_URL}` || 'https://goldcoders.dev';
+const site_domain: string = `${process.env.SNOWPACK_PUBLIC_DOMAIN}` || 'goldcoders.dev';
+const display_url: string = `${process.env.SNOWPACK_PUBLIC_DISPLAY_URL}` || 'https://goldcoders.dev';
+const show_preview_links: boolean = !!process.env.SNOWPACK_PUBLIC_SHOW_PREVIEW_LINKS;
 
 let backend: CmsBackend = {
     name: backendType,
