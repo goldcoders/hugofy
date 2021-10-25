056eaf4b80c7e9562b5d8589caa9daee2814802e
Goldcoders <goldcoders@protonmail.com>
2021-10-25 12:08:03 +0800

    added privacy config (hugo), fb chat config

--- src/collections/configs.ts
+++ src/collections/configs.ts
@@ -1,19 +1,22 @@
-import type { CmsCollection } from 'netlify-cms-core';
-import { AppConfig, MenuConfig, ParamConfig } from "../config";
-import { CartData, SocialData } from "../data";
 
-let configs:CmsCollection= {
-    "name": "configs",
-    "label": "Configurations",
-    "editor": {
-        "preview": false
-    },
-    "files": [
-        AppConfig,
-        MenuConfig,
-        ParamConfig,
-        CartData,
-        SocialData,
-    ]
-};
-export { configs };
+import { AppConfig, MenuConfig, ParamConfig, PrivacyConfig } from '../config'
+import { CartData, FbChatPlugin, SocialData } from '../data'
+import type { CmsCollection } from 'netlify-cms-core'
+
+const configs:CmsCollection = {
+  name: 'configs',
+  label: 'Configurations',
+  editor: {
+    preview: false
+  },
+  files: [
+    AppConfig,
+    PrivacyConfig,
+    MenuConfig,
+    ParamConfig,
+    FbChatPlugin,
+    CartData,
+    SocialData
+  ]
+}
+export { configs }
