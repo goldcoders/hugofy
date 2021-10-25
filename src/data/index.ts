0b45b26e2a8ac0c55efc5865de26bb9c78279bf9
Goldcoders <goldcoders@protonmail.com>
2021-10-25 12:11:18 +0800

    added fb chat plugin

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,9 +1,9 @@
-import CartData from './cart';
-import CategoryData from './categories';
-import ColorPickerData from './colorpicker';
-import FontData from './font';
-import LegalitiesData from './legalities';
-import SocialData from './social';
-import TagsData from './tags';
-export { CartData, SocialData, FontData, ColorPickerData, LegalitiesData, TagsData, CategoryData };
-
+import CartData from './cart'
+import CategoryData from './categories'
+import ColorPickerData from './colorpicker'
+import FbChatPlugin from './fb_chat_plugin'
+import FontData from './font'
+import LegalitiesData from './legalities'
+import SocialData from './social'
+import TagsData from './tags'
+export { CartData, SocialData, FbChatPlugin, FontData, ColorPickerData, LegalitiesData, TagsData, CategoryData }
