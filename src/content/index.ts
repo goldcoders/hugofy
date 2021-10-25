7dbe72f22a668bb6e9b8fc1e274109b4602fa780
Goldcoders <goldcoders@protonmail.com>
2021-10-25 12:10:31 +0800

    eslint fix

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,7 +1,6 @@
-import BlogContent from './blog';
-import ContactContent from './contact';
-import FaqContent from './faq';
-import LegalitiesContent from './legalities';
-import ProductContent from './product';
-export { BlogContent, ProductContent, FaqContent, ContactContent, LegalitiesContent };
-
+import BlogContent from './blog'
+import ContactContent from './contact'
+import FaqContent from './faq'
+import LegalitiesContent from './legalities'
+import ProductContent from './product'
+export { BlogContent, ProductContent, FaqContent, ContactContent, LegalitiesContent }
